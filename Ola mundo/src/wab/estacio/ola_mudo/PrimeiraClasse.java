0496511dfe2210c0098b89931b9b89d48deb9877
WilsonAlexandreBaez <wab.estacio@gmail.com>
2017-08-24 19:39:21 -0400

    Exercicio de Primeira Classe

--- Ola mundo/src/wab/estacio/ola_mudo/PrimeiraClasse.java
+++ Ola mundo/src/wab/estacio/ola_mudo/PrimeiraClasse.java
@@ -16,10 +16,44 @@ public class PrimeiraClasse {
      */
     public static void main(String[] args) {
         
-        int X = 10;
-        if (X == 10)
-        
-        System.out.println ("Ola mundo!!");
+      int valorDeint = 10;
+     
+      boolean verdadeiro=true, falso=false;
+      
+      
+      char valorUnico = 'M';
+
+      
+      byte valorDebit = 120;
+  
+      
+      short valorDeshort = 32767;
+    
+      
+      long valorDelong = 75769698;
+    
+      
+      float valorDfloat = 2.78f;
+  
+      
+      double valorDeduble = 84844646.6868868;
+ 
+      
+      String palavra = "sou um String";
+      
+      
+      if (valorDeint == 10){
+          
+          System.out.println("Esse e o valor boolean " + verdadeiro + " TESTE " + falso);
+          System.out.println("Valor de Char " + valorUnico);
+          System.out.println("valor de byte " + valorDebit);
+          System.out.println("Valor de short " + valorDeshort);
+          System.out.println("Valor de long " + valorDelong);
+          System.out.println("Valor de Float " + valorDfloat);
+          System.out.println("Valor de double " + valorDeduble);
+          System.out.println("Ola mundo!!");
+          System.out.println(palavra);
+      }
     }
     
 }
