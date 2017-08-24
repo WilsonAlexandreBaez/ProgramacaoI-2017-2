a03729fc4f19fd46606fb79814179e9b61d7528f
WilsonAlexandreBaez <wab.estacio@gmail.com>
2017-08-24 19:37:23 -0400

    Exercicio de variaveis

---
+++ Ola mundo/src/wab/estacio/ola_mudo/DeclaracaoDeVariaveis.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wab.estacio.ola_mudo;
+
+/**
+ *
+ * @author walexbaez
+ */
+public class DeclaracaoDeVariaveis{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      int valorDeint = 10;
+     
+      boolean verdadeiro=true, falso=false;
+      
+      
+      char valorUnico = 'M';
+
+      
+      byte valorDebit = 120;
+  
+      
+      short valorDeshort = 32767;
+    
+      
+      long valorDelong = 75769698;
+    
+      
+      float valorDfloat = 2.78f;
+  
+      
+      double valorDeduble = 84844646.6868868;
+ 
+      
+      String palavra = "sou um String";
+      
+      
+      if (valorDeint == 10){
+          
+          System.out.println("Esse e o valor boolean " + verdadeiro + " TESTE " + falso);
+          System.out.println("Valor de Char " + valorUnico);
+          System.out.println("valor de byte " + valorDebit);
+          System.out.println("Valor de short " + valorDeshort);
+          System.out.println("Valor de long " + valorDelong);
+          System.out.println("Valor de Float " + valorDfloat);
+          System.out.println("Valor de double " + valorDeduble);
+          System.out.println("Ola mundo!!");
+          System.out.println(palavra);
+      }
+    }
+    
+}
